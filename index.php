00c5f1cf4f8216f064ef74916bb8349787d4c251
Usernames <Usernames@957GS62.ad.shared>
2018-01-29 15:49:02 +0700

    edit

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ if (!is_null($events['events'])) {
 			$source = $event['source'];
 			$userID = $source['userId'];
 		
-		/*	// Send to web service
+			// Send to web service
 			$url = 'http://13.228.191.82';
 			$data = [
 				'userID' => $userID;
@@ -138,14 +138,14 @@ if (!is_null($events['events'])) {
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		//	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		//	$result = curl_exec($ch);
 			curl_close($ch);
 			
 			
-			*/
+			
 			
 			
 			
