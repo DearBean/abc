e11f6013c04e18615cc5b44ebca1939e7e9caf06
Usernames <Usernames@957GS62.ad.shared>
2018-01-29 10:13:59 +0700

    edit

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ if (!is_null($profile['events'])) {
 			// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => "Respond :" . $event['displayName']
+					'text' => "Respond :" . $profile['displayName']
 				];
 			
 			replyToUser($replyToken,$messages,$access_token);
