28c19aef2a83d84693d95b0aa4ff9db61d816fff
Usernames <Usernames@957GS62.ad.shared>
2018-01-29 15:54:49 +0700

    edit

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		//	$result = curl_exec($ch);
+			$result = curl_exec($ch);
 			curl_close($ch);
 			
 			
