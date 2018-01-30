7a5a0d30640e77a3e17ef926408ed267b7096608
Usernames <Usernames@957GS62.ad.shared>
2018-01-30 16:08:16 +0700

    connection to eb

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ if (!is_null($events['events'])) {
 			if($res===false||!is_string($res)||!strlen($res)){
 				$messages = [
 					'type' => 'text',
-					'text' => "\nFALSE " 
+					'text' => "\nFALSE " . curl_error($res)
 				];
 			}else{
 				
